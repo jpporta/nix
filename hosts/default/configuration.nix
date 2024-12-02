d8d15ed3bc3e626d93e7cd96454ed3b6e45e4107
jpporta <jpedro.porta@gmail.com>
2024-12-02 11:27:47 -0300

    keyboard

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -5,39 +5,32 @@
 { config, pkgs, inputs, lib, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-	../../modules/hyprland
-	../../modules/devtools
-inputs.home-manager.nixosModules.default
-    ];
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules/hyprland
+    ../../modules/devtools
+    inputs.home-manager.nixosModules.default
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking = {
-  	hostName = "joao-nixos";
-		useDHCP = false;
-		interfaces = {
-				enp14s0 = {
-					useDHCP = true;
-				};
-		};
+    hostName = "joao-nixos";
+    useDHCP = false;
+    interfaces = { enp14s0 = { useDHCP = true; }; };
 
-		networkmanager.enable = true;
-		firewall = {
-				enable = true;
-		};
+    networkmanager.enable = true;
+    firewall = { enable = true; };
 
-		defaultGateway = {
-				address = "192.169.3.1";
-				interface = "enp14s0";
-		};
-	};
+    defaultGateway = {
+      address = "192.169.3.1";
+      interface = "enp14s0";
+    };
+  };
 
-	nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
@@ -62,26 +55,21 @@ inputs.home-manager.nixosModules.default
     layout = "us";
     variant = "";
   };
-programs.zsh.enable = true;
+  programs.zsh.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-users.users.jpporta = {
-	isNormalUser = true;
-	description = "Joao Porta";
-	extraGroups = [ "networkmanager" "wheel" "kvm" "libvirtd" "docker" ];
-	shell = pkgs.zsh;
-	packages = with pkgs; [
-	firefox
-	neovim
-	];
-};
+  users.users.jpporta = {
+    isNormalUser = true;
+    description = "Joao Porta";
+    extraGroups = [ "networkmanager" "wheel" "kvm" "libvirtd" "docker" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [ firefox neovim ];
+  };
 
-home-manager = {
-	extraSpecialArgs = { inherit inputs; };
-	users = {
-		"jpporta" = import ./home.nix;
-	};
-};
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = { "jpporta" = import ./home.nix; };
+  };
 
   # Enable automatic login for the user.
   services.getty.autologinUser = "jpporta";
@@ -91,39 +79,19 @@ home-manager = {
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-vim
-git
-wget
-xfce.thunar
-killall
-  ];
+  environment.systemPackages = with pkgs; [ vim git wget xfce.thunar killall ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Keyboard
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = [ "*" ];
+        settings = { main = { esc = "overload(control, esc)"; }; };
+      };
+    };
+  };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
